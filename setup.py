18abf2a4c5dedbebe1a25ef9a9270ef61c8e7cb8
Edu <edu12due@gmail.com>
2023-03-03 17:52:59 +0100

    build py package

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="",
+    name="pool-playing-robot",
     version="0.0.1",
     author="Eduard Almar",
     author_email="eduard.almar.oliva@gmail.com",
-    description="A package for playing pool",
+    description="A package for pool playing robot",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/opticsensors/pool-playing-robot",
     project_urls={
-        "Bug Tracker": "",
+        "Bug Tracker": "https://github.com/opticsensors/pool-playing-robot/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
