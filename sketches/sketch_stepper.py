e4392204777cd11de3d007bc9c7056e76852d2ae
Edu AO <edu12due@gmail.com>
2023-04-17 02:02:31 +0200

    working arduino code

--- sketches/sketch_stepper.py
+++ sketches/sketch_stepper.py
@@ -7,7 +7,7 @@ stp=Stepper(baudRate=9600,
 
 stp.setupSerial()
 
-data = [(-1,0,0),(0,-1800,2700),(0,0,0),(-2,0,0),(-1,0,0),(-2,0,0)]
+data = [(-1,0,0),(-2,0,0)]
 count=0
 
 data_to_send=data[count]
