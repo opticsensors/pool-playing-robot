5f47a8168ad3d53cb198c0fc68dc1100b254de52
Edu <edu12due@gmail.com>
2023-07-06 15:17:32 +0200

    added error between ball detection and ground truth + removed preprocessing folder in examples

--- examples/prespective_transform/img_vs_point_transform_balls.py
+++ examples/prespective_transform/img_vs_point_transform_balls.py
@@ -25,9 +25,6 @@ d_centroids_undist_warp2 = {}
 for ball_num,point in d_centroids_undist.items():
     d_centroids_undist_warp2[ball_num] = eye.transform_point_given_a_matrix(point, undist_matrix)
 
-# compute differences
-d_errors = {x: np.linalg.norm(np.array(d_centroids_undist_warp[x]) - np.array(d_centroids_undist_warp2[x])) for x in d_centroids_undist_warp if x in d_centroids_undist_warp2}
-
 def debug(img, d_centroids, c):
     for ball_num in d_centroids:
         x,y=d_centroids[ball_num]
@@ -36,3 +33,24 @@ def debug(img, d_centroids, c):
 img1 = debug(img_undist_warp,d_centroids_undist_warp, (0,255,0))
 img2 = debug(img1,d_centroids_undist_warp2,(0,0,255))
 cv2.imwrite('./results/balls_undist_warp.jpg', img2)
+
+gt_all_balls=np.zeros_like(img_undist_warp)
+d_real_centroids={}
+for img_name in [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]:
+    img_ball=cv2.imread(f'./results/ground_truth_img_0/{img_name}.png')
+    M = cv2.moments(img_ball[...,0])
+    # calculate x,y coordinate of center
+    centroid= [(M["m10"] / M["m00"]),(M["m01"] / M["m00"])]
+    d_real_centroids[img_name]=centroid
+    gt_all_balls += img_ball
+
+cv2.imwrite('./results/ground_truth_img_0/all_balls.png', gt_all_balls)
+
+# compute differences
+d_errors1 = {x: np.linalg.norm(np.array(d_centroids_undist_warp[x]) - np.array(d_real_centroids[x])) for x in d_centroids_undist_warp if x in d_real_centroids}
+d_errors2 = {x: np.linalg.norm(np.array(d_centroids_undist_warp2[x]) - np.array(d_real_centroids[x])) for x in d_centroids_undist_warp2 if x in d_real_centroids}
+
+print('errors1: ',d_errors1)
+print('mean1: ', np.mean(list(d_errors1.values())))
+print('errors2: ',d_errors2)
+print('mean1: ', np.mean(list(d_errors2.values())))
--- examples/prespective_transform/preprocessed_image.py
+++ examples/prespective_transform/preprocessed_image.py
@@ -1,12 +1,11 @@
 import cv2
 from pool.eye import Eye
-import numpy as np
 
 #CV algorithms initialization
 eye=Eye()
 
 # We undistort the image
-img            = cv2.imread(f'./results/corners_0.jpg')
+img            = cv2.imread(f'./results/img_0.jpg')
 undist_img     = eye.undistort_image(img,remapping=False)
 corners = eye.get_pool_corners(img)
 undist_corners = eye.get_pool_corners(undist_img)
@@ -15,5 +14,6 @@ undist_prespective_matrix = eye.calculate_perspective_matrix(undist_corners)
 warp_distorted   = eye.transform_image_given_a_matrix(img,corners,prespective_matrix)
 warp_undistorted   = eye.transform_image_given_a_matrix(undist_img,undist_corners,undist_prespective_matrix)
 
-cv2.imwrite('./results/warp_corners_dist.jpg', warp_distorted)
-cv2.imwrite('./results/warp_corners_undist.jpg', warp_undistorted)
+cv2.imwrite('./results/img_0_undist.jpg', undist_img)
+cv2.imwrite('./results/img_0_dist_warp.jpg', warp_distorted)
+cv2.imwrite('./results/img_0_undist_warp.jpg', warp_undistorted)
