1b8f4b192db53e261b0dbb1132ae86ae0af08a7e
Edu <edu12due@gmail.com>
2023-07-10 20:42:44 +0200

    cue ball tracking

--- examples/tracking/cue_ball_tracking.py
+++ examples/tracking/cue_ball_tracking.py
@@ -1,45 +1,40 @@
 import cv2
 import numpy as np
+from pool.eye import Eye
 
-cap=cv2.VideoCapture(0,cv2.CAP_DSHOW)
+eye=Eye()
+bg_subtractor=cv2.createBackgroundSubtractorMOG2(detectShadows=True)
+frames_to_skip=0
+learning_rate=0.0
+kernel_size=(25,25)
 
-cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
-#cap.set(cv2.CAP_PROP_BRIGHTNESS, 120)
-#cap.set(cv2.CAP_PROP_EXPOSURE, -8)
+cap=cv2.VideoCapture('results/20230710_192549.mp4')
+for _ in range(frames_to_skip+1):
+    ret0, frame0 = cap.read()
 
-frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-cap.set(3,640)
-cap.set(4,480)
-
-fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-out = cv2.VideoWriter('./results/output.mp4', fourcc, 60.0, (640,480))
+corners0 = eye.get_pool_corners(frame0)
+matrix0 = eye.calculate_perspective_matrix(corners0)
 
 while(True):
     ret, frame = cap.read()
-    blur = cv2.GaussianBlur(frame,(5,5),0)
-    hsv=cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-    lower = np.array([0, 0, 0])
-    upper = np.array([255, 255, 255])
-    mask = cv2.inRange(hsv, lower, upper)
+    transf = eye.transform_image_given_a_matrix(frame, corners0, matrix0)
+    result = cv2.cvtColor(transf, cv2.COLOR_BGR2GRAY)
+    result = cv2.GaussianBlur(result, kernel_size, 0)
+    result = bg_subtractor.apply(result, learningRate=learning_rate)
 
-    contours, hier = cv2.findContours(mask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    #contours = contours[0] if len(contours) == 2 else contours[1]
+    contours, hier = cv2.findContours(result, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     if len(contours) > 0:
         c = max(contours, key = cv2.contourArea)
-        if c>100:
-            M = cv2.moments(c)
-            cx = int(M["m10"] / M["m00"])
-            cy = int(M["m01"] / M["m00"])
-            cv2.circle(frame,(cx,cy),5,(0,0,255),-1)
+        M = cv2.moments(c)
+        cx = int(M["m10"] / M["m00"])
+        cy = int(M["m01"] / M["m00"])
+        cv2.circle(transf,(cx,cy),8,(255,0,255),-1)
 
-    out.write(frame)
-    cv2.imshow('frame', frame)
+    transf = cv2.resize(transf, (0,0), fx=0.6, fy=0.6)
+    cv2.imshow('transf', transf)
     c = cv2.waitKey(1)
     if c & 0xFF == ord('q'):
         break
 
 cap.release()
-out.release()
 cv2.destroyAllWindows()
